eab2ebd098920664886719b8e7f7e73f82731258
AI1411 <goethe0719@gmail.com>
2019-10-19 15:04:00 +0900

    リレーション追加

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
 }
